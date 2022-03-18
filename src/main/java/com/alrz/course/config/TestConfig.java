73b628742efe226c37bd53f42e80e4ae9816bece
Alvaro Dormundo <adormundo@gmail.com>
2022-03-17 22:04:06 -0300

    Category

--- src/main/java/com/alrz/course/config/TestConfig.java
+++ src/main/java/com/alrz/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.alrz.course.config;
 
+import com.alrz.course.entities.Category;
 import com.alrz.course.entities.Order;
 import com.alrz.course.entities.User;
 import com.alrz.course.entities.enums.OrderStatus;
+import com.alrz.course.repositories.CategoryRepository;
 import com.alrz.course.repositories.OrderRepository;
 import com.alrz.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
@@ -33,6 +38,11 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WAITING_PAYMENT);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.WAITING_PAYMENT);
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
     }
 }
---
+++ src/main/java/com/alrz/course/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.alrz.course.repositories;
+
+import com.alrz.course.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
